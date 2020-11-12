dde91b4cf46f5276e7d496ebae6730e05cb77b0f
Marco Paggioro <marco.paggioro@gmail.com>
2020-11-12 23:46:49 +0100

    CQI

--- src/main/java/it/areson/aresondeathswap/MessageManager.java
+++ src/main/java/it/areson/aresondeathswap/MessageManager.java
@@ -11,16 +11,19 @@ import java.util.Objects;
 
 public class MessageManager extends FileManager {
 
+    private final String prefix;
+
     public MessageManager(AresonDeathSwap plugin, String fileName) {
         super(plugin, fileName);
+        prefix = getFileConfiguration().getString("prefix");
     }
 
     public void sendPlainMessage(Player player, String messageKey) {
         String message = getFileConfiguration().getString(messageKey);
         if (Objects.nonNull(message)) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message));
         } else {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cError: '" + messageKey + "' message does not exists!"));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&cError: '" + messageKey + "' message does not exists!"));
         }
     }
 
@@ -30,16 +33,16 @@ public class MessageManager extends FileManager {
             BaseComponent[] rawMessage = ComponentSerializer.parse(message);
             player.spigot().sendMessage(rawMessage);
         } else {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cError: '" + messageKey + "' message does not exists!"));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&cError: '" + messageKey + "' message does not exists!"));
         }
     }
 
     public String getPlainMessage(String messageKey) {
         String message = getFileConfiguration().getString(messageKey);
         if (Objects.nonNull(message)) {
-            return ChatColor.translateAlternateColorCodes('&', message);
+            return ChatColor.translateAlternateColorCodes('&', prefix + message);
         } else {
-            return ChatColor.translateAlternateColorCodes('&', "&cError: '" + messageKey + "' message does not exists!");
+            return ChatColor.translateAlternateColorCodes('&', prefix + "&cError: '" + messageKey + "' message does not exists!");
         }
     }
 
@@ -48,7 +51,7 @@ public class MessageManager extends FileManager {
         if (Objects.nonNull(message)) {
             return ComponentSerializer.parse(message);
         } else {
-            return TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', "&cError: '" + messageKey + "' message does not exists!"));
+            return TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', prefix + "&cError: '" + messageKey + "' message does not exists!"));
         }
     }
 }
