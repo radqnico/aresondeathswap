3d5e8bc788d2bd3bac2cc0b93049dcf9473f6888
nikoo <nicoloscialpi@gmail.com>
2020-11-16 19:10:42 +0100

    Added some sounds #4

--- src/main/java/it/areson/aresondeathswap/managers/SoundManager.java
+++ src/main/java/it/areson/aresondeathswap/managers/SoundManager.java
@@ -15,6 +15,10 @@ public class SoundManager {
         }
     }
 
+    public void joinArena(Player target) {
+        target.playSound(target.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.MASTER, 1f, 1f);
+    }
+
     public void cannotJoinArena(Player target) {
         target.playSound(target.getLocation(), Sound.ENTITY_VILLAGER_NO, SoundCategory.MASTER, 0.5f, 1f);
     }
@@ -28,11 +32,11 @@ public class SoundManager {
     }
 
     public void gameStarted(Player target) {
-        target.playSound(target.getLocation(), Sound.ENTITY_EVOKER_PREPARE_SUMMON, SoundCategory.MASTER, 0.4f, 1f);
+        target.playSound(target.getLocation(), Sound.ENTITY_WITHER_SPAWN, SoundCategory.MASTER, 0.4f, 1f);
     }
 
     public void loser(Player target) {
-        target.playSound(target.getLocation(), Sound.ENTITY_RAVAGER_STUNNED, SoundCategory.MASTER, 0.4f, 1f);
+        target.playSound(target.getLocation(), Sound.ENTITY_MULE_AMBIENT, SoundCategory.MASTER, 0.4f, 1f);
     }
 
     public void winner(Player target) {
@@ -44,6 +48,6 @@ public class SoundManager {
     }
 
     public void teleport(Player target) {
-        target.playSound(target.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, SoundCategory.MASTER, 0.6f, 1f);
+        target.playSound(target.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, SoundCategory.MASTER, 0.6f, 1f);
     }
 }
