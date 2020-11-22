0ff5ebc336e18576b4f03a2d853a58eca4350fa1
Marco Paggioro <marco.paggioro@gmail.com>
2020-11-22 11:11:20 +0100

    CQI

--- src/main/java/it/areson/aresondeathswap/AresonDeathSwap.java
+++ src/main/java/it/areson/aresondeathswap/AresonDeathSwap.java
@@ -12,7 +12,6 @@ import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.List;
