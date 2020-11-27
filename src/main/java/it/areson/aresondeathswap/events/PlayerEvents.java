100b2e3f62e2ebde1cf4235c75565063ebf0c1e6
Marco Paggioro <marco.paggioro@gmail.com>
2020-11-27 21:25:42 +0100

    Fixes

--- src/main/java/it/areson/aresondeathswap/events/PlayerEvents.java
+++ src/main/java/it/areson/aresondeathswap/events/PlayerEvents.java
@@ -71,18 +71,19 @@ public class PlayerEvents implements Listener {
 
     @EventHandler
     public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
-        World playerWorld = event.getPlayer().getLocation().getWorld();
-
-        if (playerWorld != null) {
-//            ArrayList<Player> players = new ArrayList<>(event.getRecipients());
-//            players.forEach(player -> {
-//                World targetWorld = player.getLocation().getWorld();
-//                if (targetWorld != null && !targetWorld.getName().equals(playerWorld.getName())) {
-//                    event.getRecipients().remove(player);
-//                }
-//            });
-            event.setFormat("Ciao" + event.getFormat());
-        }
+        event.setFormat("[prefix] " + event.getFormat());
+//        World playerWorld = event.getPlayer().getLocation().getWorld();
+//
+//        if (playerWorld != null) {
+////            ArrayList<Player> players = new ArrayList<>(event.getRecipients());
+////            players.forEach(player -> {
+////                World targetWorld = player.getLocation().getWorld();
+////                if (targetWorld != null && !targetWorld.getName().equals(playerWorld.getName())) {
+////                    event.getRecipients().remove(player);
+////                }
+////            });
+//            event.setFormat("Ciao" + event.getFormat());
+//        }
     }
 
     @EventHandler
